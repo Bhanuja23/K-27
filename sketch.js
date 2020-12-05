06a963656275aaed1b33cc6b83963cbb04186773
Bhanuja23 <71247672+Bhanuja23@users.noreply.github.com>
2020-12-05 19:50:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
@@ -14,15 +15,18 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 	roof1 = new Roof(400,50,600,60)
-	bob1 = new bob(200,500,100)
-    bob2 = new bob(300,500,100)
-	bob3 = new bob(400,500,100)
-	bob4 = new bob(500,500,100)
-	bob5 = new bob(600,500,100)
+	bob1 = new bob(200,500,30)
+    bob2 = new bob(300,500,30)
+	bob3 = new bob(400,500,30)
+	bob4 = new bob(500,500,30)
+	bob5 = new bob(600,500,30)
 	
-    ropeobj = new Rope (bob1.body,roof1.body,-200,10)
+    ropeobj = new Rope (bob1.body,roof1.body,-250,10)
 	
-    ropeobj1 = new Rope (bob2.body,roof1.body,-100,10)
+    ropeobj1 = new Rope (bob2.body,roof1.body,-150,10)
+    ropeobj2 = new Rope (bob3.body,roof1.body,-50,10)
+    ropeobj3 = new Rope (bob4.body,roof1.body,50,10)
+  ropeobj4 = new Rope (bob5.body,roof1.body,140,10)
 	Engine.run(engine);
   
 }
@@ -43,13 +47,27 @@ function draw() {
 
  ropeobj.display();
  ropeobj1.display();
-
+ ropeobj2.display();
+ ropeobj3.display();
+ ropeobj4.display();
+ 
 
  text(mouseX + "," + mouseY, mouseX, mouseY)
   
 
   drawSprites();
   
+}
+
+
+
+function keyPressed(){
+  if(keyCode === UP_ARROW){
+    //bob5
+  Matter.Body.applyForce(bob5.body,bob5.body.position,{x:500,y:600})
+  //bob1
+  //Matter.Body.applyForce(bob5.body,bob5.body.position,{x:-500,y:-600})
+}
 }
 
 
